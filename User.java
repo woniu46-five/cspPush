ce947cfce4bb1552cbaab7111f07ff0884c08383
chen-pengge <51945024+chen-pengge@users.noreply.github.com>
2019-06-18 17:58:34 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,41 @@
+package com.project.bean;
+
+import org.springframework.stereotype.Component;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String pass;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass == null ? null : pass.trim();
+    }
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", pass=" + pass + "]";
+	}
+    
+}
